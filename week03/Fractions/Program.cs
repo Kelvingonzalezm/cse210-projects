aaa738cd6128ea271adc543720daa6a155e22bb8
Kelvin Gonzalez <kelvingonzalezm@gmail.com>
2026-01-19 17:46:59 -0400

    Add Fraction class and functional Program

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction first = new Fraction();
+        ShowFraction(first);
+
+        Fraction second = new Fraction(5);
+        ShowFraction(second);
+
+        Fraction third = new Fraction(3, 4);
+        ShowFraction(third);
+
+        Fraction fourth = new Fraction(1, 3);
+        ShowFraction(fourth);
+
+    }
+
+    static void ShowFraction(Fraction fraction)
+    {
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
